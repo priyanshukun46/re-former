1be55e3a07e635770cc4ce63544554d5a10dd846
priyanshukun46 <pkfb46@gmail.com>
2026-07-07 00:51:57 +0530

    gg

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create]
+  resources :users
   
   get "up" => "rails/health#show", as: :rails_health_check
 
